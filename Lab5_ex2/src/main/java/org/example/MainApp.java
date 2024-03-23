b7e016ca9e8ef1daa320b5acd2bd9819610c8ed5
daniciucut <cuciceadaniel@icloud.com>
2024-03-23 08:15:09 +0200

    LABORATOR 4

---
+++ Lab5_ex2/src/main/java/org/example/MainApp.java
@@ -0,0 +1,20 @@
+package org.example;
+
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.List;
+
+
+public class MainApp {
+    public static void main(String[] args) {
+
+
+
+    }
+
+
+
+
+}
